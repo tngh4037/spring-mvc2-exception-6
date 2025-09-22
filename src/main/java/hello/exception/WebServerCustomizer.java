493d95b58ca3e7fbb4952db14683be5727d63204
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-09-23 06:31:10 +0900

    주석 추가

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -6,7 +6,8 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-// @Component // 주석처리 하게 되면, 스프링 부트가 제공하는 기본 오류 매커니즘이 동작한다. (?) (BasicErrorController)
+// @Component // 주석처리 하게 되면, 스프링 부트가 제공하는 기본 오류 매커니즘이 동작한다. (BasicErrorController)
+// 참고로, WebServerCustomizer 를 사용해서 직접 등록한다고 해서, BasicErrorController 가 동작하지 않는게 아니다. WebServerCustomizer 에서 설정한 오류 코드에 따른 오류 페이지가 우선적으로 적용되고, 여기서 설정되지 않은 오류에 대해서는 BasicErrorController 가 동작한다.
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> { // pom.xml
 
     // 이름 그대로 웹 서버를 커스터마이징 하는 것이다.
